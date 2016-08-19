defc09e1e88d5ffbb674b898680b146a29dcca1a
ZeSVeN <ZeSVeN7@gmail.com>
2016-08-19 16:43:08 +0700

    1. Добавлено условие, если дискриминант равен нулю. 2. Переименованна переменная d в discriminant.

--- QuadraticEquation/script.js
+++ QuadraticEquation/script.js
@@ -1,14 +1,23 @@
 var a = 2;
-var b = 5;
+var b = 4;
 var c = 2;
 
-var d = b * b - 4 * a * c;
+var discriminant = b * b - 4 * a * c;
 
-if (d < 0) {
+if (discriminant < 0) {
+
     console.log("Нет корней");
+
+} else if (discriminant === 0){
+
+    var x = -b / (2 * a);
+    console.log(x);
+
 } else {
-    var x1 = ( -b - Math.sqrt(d)) / (2 * a);
-    var x2 = ( -b + Math.sqrt(d)) / (2 * a);
+
+    var x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    var x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
     console.log(x1);
     console.log(x2);
+
 }
