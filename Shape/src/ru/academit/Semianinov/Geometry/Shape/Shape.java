a8fbb9b79087f63a21dadbf8c51961a4c6a4a8e2
ZeSVeN <ZeSVeN7@gmail.com>
2016-11-08 00:01:35 +0700

    Shape

---
+++ Shape/src/ru/academit/Semianinov/Geometry/Shape/Shape.java
@@ -0,0 +1,13 @@
+package ru.academit.Semianinov.Geometry.Shape;
+
+
+public interface Shape {
+
+    double getWidth();
+
+    double getHeight();
+
+    double getArea();
+
+    double getPerimeter();
+}
