b8d35d4f8582e70c0f0054cbbb2133dc941d0bb0
ZeSVeN <ZeSVeN7@gmail.com>
2016-11-12 14:47:55 +0700

    Объявлены методы: 1) add 2) remove 3) multiplication 4) revers Переопределены методы equals и hashcode.

--- Vector/src/ru/academit/semianinov/vector/Main.java
+++ Vector/src/ru/academit/semianinov/vector/Main.java
@@ -4,20 +4,28 @@ package ru.academit.semianinov.vector;
 public class Main {
     public static void main(String[] args) {
 
-        double[] array = {1, 6, 5, 3, 6, 7, 8};
-
         Vector vector = new Vector(3);
 
         System.out.println(vector.getSize());
 
+        double[] array = {1, 6, 5, 3, 6, 7, 8};
         Vector vector1 = new Vector(array);
 
         Vector vector2 = new Vector(vector1);
-        vector2.toString();
-        System.out.println(vector2);
+
+        vector2.setValue(0,1.00001);
+        System.out.println(vector1.equals(vector2));
 
         Vector vector3 = new Vector(10, array);
-        vector3.toString();
-        System.out.println(vector3);
+        vector3.multiplication(2);
+
+        vector1.add(vector2);
+
+        vector1.revers();
+
+        vector1.remove(vector2);
+
+        vector1.setValue(vector1.getSize()-1, 0);
+
     }
 }
--- Vector/src/ru/academit/semianinov/vector/Vector.java
+++ Vector/src/ru/academit/semianinov/vector/Vector.java
@@ -1,6 +1,6 @@
 package ru.academit.semianinov.vector;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Vector {
 
@@ -50,9 +50,73 @@ class Vector {
             string.append(e)
                     .append(", ");
         }
-        string.deleteCharAt(string.length() - 1);
-        string.append(" }");
+        string.delete(string.length() - 2, string.length() - 1);
+        string.append("}");
 
         return string.toString();
+    }
+
+    public void add(Vector vector) {
+
+        int newSize = this.array.length + vector.getSize();
+        double[] newArray = new double[newSize];
+
+        System.arraycopy(this.array, 0, newArray, 0, this.array.length);
+        System.arraycopy(vector.array, 0, newArray, this.array.length, vector.getSize());
+
+        this.array = newArray;
+    }
+
+    public void remove(Vector vector) {
+
+        int newSize = this.array.length - vector.getSize();
+        double[] newArray = new double[newSize];
+
+        System.arraycopy(this.array, 0, newArray, 0, newSize);
+        this.array = newArray;
+    }
+
+    public void multiplication(double number) {
+        for (int i = 0; i < this.array.length; ++i) {
+            array[i] = array[i] * number;
+        }
+    }
+
+    public void revers() {
+        for (int i = 0; i < this.array.length; ++i) {
+            array[i] = array[i] * (-1);
+        }
+    }
+
+    public void setValue(int index, double value) {
+        array[index] = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vector vector = (Vector) o;
+
+        if (this.array.length == vector.getSize()) {
+
+            double accuracy = 0.0001;
+
+            for (int i = 0; i < array.length; ++i) {
+                if (Math.abs(this.array[i] - vector.array[i]) > accuracy) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 23;
+        int hash = 1;
+        return hash * prime + Arrays.hashCode(array);
     }
 }
